4fa56f81f913e3528fb9b2e7f410959790826a2c
BeNikk <bhattnik442@gmail.com>
2025-04-21 21:16:01 +0530

    build

--- italian/src/App.tsx
+++ italian/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import  { useState, useEffect, useCallback } from 'react';
 import { motion } from 'framer-motion';
 import useAudio from './hooks/useAudio';
 import { getRandomCharacters, getRandomCharacter, soundEffects } from './data/characters';
