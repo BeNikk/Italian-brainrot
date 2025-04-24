cc79392a26ead78cdcc68826e25fba557b13beb8
BeNikk <bhattnik442@gmail.com>
2025-04-24 12:29:31 +0530

    images

--- italian/src/data/characters.ts
+++ italian/src/data/characters.ts
@@ -1,12 +1,11 @@
 import { Character } from '../types';
 
-// Mock character data
 export const characters: Character[] = [
   {
     id: 1,
     name: "La Vaca Saturno Saturnita",
     powerLevel: 85,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fnuypNEsnlykQfhdXxAJNQAW_tyu-of7TvCCaEklLziEtaE-hMRzPaJBYkWt2qvRVM7IyzxlFjhriqa3DhecIvFCGIwkwI1QXAOK-xwzXPODcFZxL5SuztnBofSbk-YCM4IZlTnG41A4etI2wzOp1hz8.webp",
+    image: "/Saturno_Saturnita.webp",
     audio: "/saturno.mp3",
     description: "It is a character with a person's feet and a cow's head on Saturn 's body. It appears at the festival site, and every time I take a step, it looks like a dance and makes people enjoy it. Like the above Lyrili Larila"
   },
@@ -14,7 +13,7 @@ export const characters: Character[] = [
     id: 2,
     name: "Brr Brr Patapim",
     powerLevel: 83,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fnhw-_45X9d9iSMKAYTuQdBtu80dE7OuqITk0_HGJK_gKul4q4LjErBnKWOVrmFhrtgeX-PMClymQ6r8diwq99AzROoUgtWlJ8UaJz0xIYMUWZnCHA9oX5Tn9RABY3yaxOKcJ_f7HsfyjiK4yAJFfcIU.webp",
+    image: "/patapim.webp",
     audio: "/prrprrpatapim.mp3",
     description: "A character with the head of a cozu monkey on the limbs of the tree . Those who have invaded the forest while guarding the forest are attacked by the roots of the tree, or make a trap and attack. His real name is Pata Pim, and the previous Berr berr is the sound of the blue frog"
   },
@@ -22,7 +21,7 @@ export const characters: Character[] = [
     id: 3,
     name: "Bombardiro Crocodilo",
     powerLevel: 91,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fnn9516ikleqayW8O2Jvam-wa1Q0qJMzCsljYAtc8zUAjfAxvvTubXEQYCJ9pQ2lnT7IApNg57zYL_OzDMCjbvQp9pUpOy3WDIzVDtSuTr_Iuid4tfaeH9zwLHEDOgUe1GtwxFW4o29bWL6lcVZfRq_Q.webp",
+    image: "/crocodilo.webp",
     audio: "/bombardilo-crocodilo.mp3",
     description: "It is a character that synthesizes bombers and crocodiles . The next famous character after Tralalo Tralala in Italian Brainrot. It is said that they bomb the children in the Gaza district of Palestine because they do not believe Allah. "
   },
@@ -30,7 +29,7 @@ export const characters: Character[] = [
     id: 4,
     name: "U Din Din Din Din Dun Ma Din Din Din Dun",
     powerLevel: 88,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fnsxjxe_Lfj2kdazmXlnV5mq1FndEvzwSjfKm1swDBLLhA2EgDVnv2SyphEcLlMEdEiy0C9vCG8fBA8D50bnMNXHe6PrM4xLVOOZj78mJjdIX1E-xGTgHNU36FL8M0-HwwryiPi75uDb46oazV38A8Yc.webp",
+    image: "/dindun.webp",
     audio: "/dindindun.mp3",
     description: "It is a character that combines the muscular body that reminiscent of a bodybuilder on the orange head. The image is used by one of the above. It doesn't appear much from the vs. video, but it comes out as a strong man, and the head of Tung Tung Tung Sahur made a slightly gold"
   },
@@ -38,7 +37,7 @@ export const characters: Character[] = [
     id: 5,
     name: "Garamaraman dan Garamamaraman",
     powerLevel: 80,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fnlYZE44miohrdDc08qAgwf64UDjbBEh_1Ot-oKV3dXn8Utlr-cxkG6t7GJUkO-e9ArzHjIoJMzKc9uBWRzFvrmgBpOVTSra6pI5TgUNTSReREdMZpBWQL6FsuLZ6tZ0wzVVujRMYKlb9CUsqGpy_JUw.webp",
+    image: "/garam.webp",
     audio: "/garamaraman.mp3",
     description: "It is a character that synthesizes a person's face and feet in the salt and honey complex. The name of the salt barrel is karamarahman, and the honey barrel is Madungdong. It is said to appear mainly during the period of Ramadan ."
   },
@@ -46,7 +45,7 @@ export const characters: Character[] = [
     id: 6,
     name: "Lirilì Larilà",
     powerLevel: 92,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fnkRcVALcv9FHUvwt3W6ybC1tsU64mWu8Xs1WBzxlU5BrKSwTW2kPZTJMO8QtOCXscph4TEp69z4HUl2_qjHqGEGTO17PCfDGHh1fPPJ1oN59PUWzqkvz-wCZpZoS0D9wKigrJUnzgbSdQ7tzkJ9Hq2k.webp",
+    image: "/Lirili_rili_ralila.webp",
     audio: "/lirililarila.mp3",
     description: "Elephant characters with cactuses wearing sandals. It is often described as using the clock that can stop the time in the battle. It is mainly expressed by avoiding the fight, but in the technical fight using elephant's unique physique and long nose, it shows the power of the strong."
   },
@@ -54,7 +53,7 @@ export const characters: Character[] = [
     id: 7,
     name: "Tralalero Tralala",
     powerLevel: 86,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fniTFU6Y0DPiOoNPHtXbn0dISNiS3wdrErT_kZKOT7qO_TUmqpoO-AXdnFfXMaasT0ZU1GYyAXy29P6LB2LugWwu4d2nDrTwdAVmX4eioCJvRoP_QpSJfWunblEgiAfdesZSmTGW17u6UEk8-AbShul0.webp",
+    image: "/tralalero.webp",
     audio: "/tralalero.mp3",
     description: "Shark characters with three legs on the beach with Nike sneakers."
   },
@@ -62,7 +61,7 @@ export const characters: Character[] = [
     id: 8,
     name: "Trippi Troppi Troppi Trappa",
     powerLevel: 85,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fnjJC3ssnDF6KBdzSb37BGErtJlCodLcKBmJXxatm136nQzXLG_pPOwE9bnT94ccG0Qu0l3S7_7oRQ2_u9pxQksfpkal81n6OTWe_ihnXdahPUuBFV0mhrzye5doJLRdJD94uQXVYCE8MSYoIKHbcOSg.webp",
+    image: "/trippi.webp",
     audio: "/tripitropi.mp3",
     description: "There are two characters with the same name, with images that combine shrimp and cats and images of fat bears and fish share their names. The entire name is 'TripPi Troppi Troppa Trippa'."
   },
@@ -70,7 +69,7 @@ export const characters: Character[] = [
     id: 9,
     name: "Tung Tung Tung Tung Tung Tung Tung Tung Tung Sahur",
     powerLevel: 90,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fnpQxoLZ8xSG2K78Ko_iyg6w9unwXe35kZht70wzyyRGGxj49pvKhxuNcLyhiKPIqph80LguKU-HR_shMEiM9l-BqQz03LUoIRRdUTV0X_w1x5N88STadMRrdfQAMXZxuAHQeWgjf9U05fUMVqKdq3F8.webp",
+    image: "/tungtung.webp",
     audio: "/tungtung.mp3",
     description: "A brown tree sculpture character holding a baseball bat. The face resembles a Thomas locomotive, causing unpleasant valley ."
   },
@@ -78,7 +77,7 @@ export const characters: Character[] = [
     id: 10,
     name: " Frigo Camelo Bufo Furdelo",
     powerLevel: 85,
-    image: "https://i.namu.wiki/i/01TdabI-F1W2mG6xKg1fniz7U70m1YN1hTYvGK453XnemJruLf5dth8h0AA7NyvHroLk0PqEFPPT1vGk3_S8uLLch-wuw5cYAI1Jal84PI22Dc4ACywatcDU1UjiITJzI0QXNX47goBERUNWAD-BCeX698TKCeFbW-K9CbrGdxs.webp",
+    image: "/frigocamelo.webp",
     audio: "/frigocamelo.mp3",
     description: "It is a camel character with the torso of the refrigerator wearing shoes. A cold wind comes out of your mouth. Sometimes it is frozen. VS videos are often applied to Boneca Ambalabu"
   }
